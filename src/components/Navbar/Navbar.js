f7febbc304d8cdb4d010e32558bbccc9f4e3041e
yogitashete2412 <yogitashete24@gmail.com>
2023-09-09 10:00:35 +0530

    navbar component created

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,2 +1,11 @@
 
 
+<nav class="bg-gray-800 p-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <a href="#" class="text-white text-xl font-semibold">Home</a>
+        <div class="space-x-4">
+            <a href="#" class="text-gray-300 hover:text-white">About</a>
+            <a href="#" class="text-gray-300 hover:text-white">Contact</a>
+        </div>
+    </div>
+</nav>
