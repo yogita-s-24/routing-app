5c37604048b44cbfe16521e1c337abd4b55e6e82
yogitashete2412 <yogitashete24@gmail.com>
2023-09-09 09:47:37 +0530

    react app created

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <h1 className="underline">Hiii</h1>
+  </>
+);
